52bede3cf53950cd4a407e3d8e9dba641e5dad12
Fhiz <support@wovencode.net>
2020-01-14 19:33:47 +0100

    Preperations for CharacterCreation

---
+++ Scripts/UI/UISelectPlayerSlot.cs
@@ -0,0 +1,81 @@
+﻿// =======================================================================================
+// Wovencore
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+using UnityEngine;
+using UnityEngine.UI;
+using wovencode;
+
+namespace wovencode
+{
+	
+	// ===================================================================================
+	// UISelectPlayerSlot
+	// ===================================================================================
+	public partial class UISelectPlayerSlot : UIButton
+	{
+		
+		[Header("UI Elements")]
+		public Text textName;
+		public Image imageSelected;
+		public Button buttonSelect;
+		
+		[Header("Used Images")]
+		public Sprite unselectedImage;
+		public Sprite selectedImage;
+		
+		protected int _index;
+		protected bool selected;
+		
+		// -------------------------------------------------------------------------------
+		// Init
+		// -------------------------------------------------------------------------------
+		public void Init(UIButtonGroup _buttonGroup, int index, string name, bool _selected=false)
+		{
+			
+			selected = _selected;
+			
+			base.Init(_buttonGroup);
+			
+			_index = index;
+			textName.text = name;
+			
+		}
+		
+		// -------------------------------------------------------------------------------
+		// OnPressed
+		// -------------------------------------------------------------------------------
+		public override void OnPressed()
+		{
+		
+			if (selected)
+			{
+				selected = false;
+				imageSelected.sprite = unselectedImage;
+				
+			}
+			else
+			{
+				selected = true;
+				imageSelected.sprite = selectedImage;
+			}
+			
+		}
+		
+		// -------------------------------------------------------------------------------
+		// Index
+		// -------------------------------------------------------------------------------
+		public int Index
+		{
+			get { return (selected) ? _index : -1; }
+		}
+				
+		// -------------------------------------------------------------------------------
+		
+	}
+
+}
+
+// =======================================================================================
---
+++ Scripts/UI/UIWindowSelectPlayer.cs
@@ -0,0 +1,71 @@
+﻿// =======================================================================================
+// Wovencore
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+using wovencode;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace wovencode
+{
+
+	// ===================================================================================
+	// UIWindowSelectPlayer
+	// ===================================================================================
+	public partial class UIWindowSelectPlayer : UIRoot
+	{
+	
+		[Header("Network")]
+		public wovencode.NetworkManager manager;
+		
+		[Header("Prefab")]
+		public UISelectPlayerSlot slotPrefab;
+		public UIButtonGroup buttonGroup;
+		
+		[Header("Content")]
+		public Transform contentViewport;
+		
+		[Header("Buttons")]
+		public Button createButton;
+		public Button selectButton;
+		public Button deleteButton;
+		public Button backButton;
+		
+		protected int index = -1;
+		
+		// -------------------------------------------------------------------------------
+		// ThrottledUpdate
+		// -------------------------------------------------------------------------------
+		protected override void ThrottledUpdate()
+		{
+			
+			if (manager.state != NetworkState.Lobby)
+			{
+				Hide();
+				return;
+			}
+			
+			//createButton.interactable = 
+			
+			
+			selectButton.interactable = (index != -1);
+			
+			
+			deleteButton.interactable = (index != -1);
+			
+			
+			
+			backButton.onClick.SetListener(() => { Hide(); });
+
+		}
+		
+		// -------------------------------------------------------------------------------
+		
+	}
+
+}
+
+// =======================================================================================
